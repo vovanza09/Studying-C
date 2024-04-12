efba8fe60715c04f0c612fd13a000edd52b67132
vovanza09 <165702276+vovanza09@users.noreply.github.com>
2024-04-12 20:48:16 +0300

    Add files via upload

---
+++ Studying-C/Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] arr = new int [5];
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 9;
+arr[3] = 3;
+arr[4] = 1;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+int[] arr2 = new int[5] {1, 2, 3, 4, 5};
+
+int arr3 = {3, 4, 5, 7, 6};
+
+// arr = [1, 5, 6, 7, 8] создание массива в питоне
